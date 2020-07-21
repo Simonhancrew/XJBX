6fcd7fb45207294bccd20950d691a43107decfa2
Simonhancrew <597494370@qq.com>
2020-07-21 14:37:36 +0800

    2020/07/21 struct Treenode

--- LeetCode/CPP/Leetcode面试04.04-树的平衡判断.h
+++ LeetCode/CPP/Leetcode面试04.04-树的平衡判断.h
@@ -1,3 +1,20 @@
+#include<stdlib.h>
+#include<algorithm>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode():val(0),left(nullptr),right(nullptr){}
+    TreeNode(int x,TreeNode* left,TreeNode* right):val(x), left(left), right(right){}
+    TreeNode(int x){
+        val = x;
+        left = nullptr;
+        right =nullptr;
+    }
+
+};
 class Solution {
     int maxDepth(TreeNode* root) {
         if(root == NULL) return 0;
