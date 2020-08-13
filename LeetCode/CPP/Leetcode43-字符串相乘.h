cb722b6bd9ef53e60b0a790205fb213ec5ae7467
Wuhan <597494370@qq.com>
2020-08-13 19:46:19 +0800

    2020/08/13 daily

--- LeetCode/CPP/Leetcode43-字符串相乘.h
+++ LeetCode/CPP/Leetcode43-字符串相乘.h
@@ -4,11 +4,39 @@
 using namespace std;
 class Solution{
 public:
-    string multiply(string &num1, string &num2) {
-        
+    string multiply(string num1, string num2) {
+        if (num1 == "0" || num2 == "0") {
+            return "0";
+        }
+        string ans = "0";
+        int m = num1.size(), n = num2.size();
+        for (int i = n - 1; i >= 0; i--) {
+            string curr;
+            int add = 0;
+            for (int j = n - 1; j > i; j--) {
+                curr.push_back(0);
+            }
+            int y = num2.at(i) - '0';
+            for (int j = m - 1; j >= 0; j--) {
+                int x = num1.at(j) - '0';
+                int product = x * y + add;
+                curr.push_back(product % 10);
+                add = product / 10;
+            }
+            while (add != 0) {
+                curr.push_back(add % 10);
+                add /= 10;
+            }
+            reverse(curr.begin(), curr.end());
+            for (auto &c : curr) {
+                c += '0';
+            }
+            ans = addStrings(ans, curr);
+        }
+        return ans;
     }
 private:
-    string add(string &num1,string &nums2){
+    string addStrings(string &num1,string &nums2){
         int n = num1.size()-1,m = num2.size()-1;
         int add = 0;
         string ans = "";
