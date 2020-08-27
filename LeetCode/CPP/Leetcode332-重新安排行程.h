1e85dd6dcdd6a903ab44ecbc5e7c38532a0836f8
Simonhancrew <597494370@qq.com>
2020-08-27 17:21:30 +0800

    backtract

--- LeetCode/CPP/Leetcode332-重新安排行程.h
+++ LeetCode/CPP/Leetcode332-重新安排行程.h
@@ -3,33 +3,33 @@
 #include <unordered_map>
 #include <map>
 using namespace std;
-class Solution{
-public:    
+class Solution {
+public:
     vector<string> findItinerary(vector<vector<string>>& tickets) {
         vector<string> result;
-        for(const vector<string> &vec:tickets){
-            flytos[vec[0]][vec[1]]++;
+        for (const vector<string>& vec : tickets) {
+            flytos[vec[0]][vec[1]]++; // 记录映射关系
         }
         result.push_back("JFK");
-        backtrack(tickets.size(),1,result);
+        backtracking(tickets.size(), 1, result);
         return result;
     }
 private:
-    unordered_map<string,map<string,int>> flytos;
-    bool backtrack(int ticketNum,int index,vector<string> result){
-        if(index == ticketNum){
+// unordered_map<出发城市, map<到达城市, 航班次数>> flytos
+    unordered_map<string, map<string, int>> flytos;
+    bool backtracking(int ticketNum, int index, vector<string>& result) {
+        if (index == ticketNum + 1) {
             return true;
         }
-        for(pair<const string,int> &flyto:flytos[result[result.size()-1]]){
-            if(flyto.second > 0){
+        for (pair<const string, int>& flyto : flytos[result[result.size() - 1]]) {
+            if (flyto.second > 0 ) { // 使用int字段来记录到达城市是否使用过了
                 result.push_back(flyto.first);
                 flyto.second--;
-                if(backtrack(ticketNum,index+1,result)) return true;
-                result.pop_back();
-                flyto.second++;
-            }
+                if (backtracking(ticketNum, index + 1, result)) return true;
+                    result.pop_back();
+                    flyto.second++;
+                }
         }
         return false;
-    }
-
+    }   
 };
