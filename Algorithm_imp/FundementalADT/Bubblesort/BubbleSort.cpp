b0a7d972a6b0399138c29b2162870e00f3106709
Simonhancrew <597494370@qq.com>
2020-09-01 16:39:14 +0800

    实现一下stl

---
+++ Algorithm_imp/FundementalADT/Bubblesort/BubbleSort.cpp
@@ -0,0 +1,16 @@
+//
+// Created by han on 2020/9/1.
+//
+#include <algorithm>
+void bubbleSort(int a[],int n){
+    int cmp = 0,swp = 0;
+    while (n--){
+        for(int i =0;i<n;i++){
+            if(a[i]>a[i+1]){
+                std::swap(a[i],a[i+1]);
+                swp++;
+            }
+            cmp++
+        }
+    }
+}
---
+++ Algorithm_imp/FundementalADT/cmake-build-debug/CMakeFiles/FundementalADT.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for FundementalADT.
+# This may be replaced when dependencies are built.
