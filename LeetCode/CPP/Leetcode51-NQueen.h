1e6bac27e6c0848692014dde4de628aa0a32ea00
Wuhan <597494370@qq.com>
2020-09-03 07:53:21 +0800

    change LT51

--- LeetCode/CPP/Leetcode51-NQueen.h
+++ LeetCode/CPP/Leetcode51-NQueen.h
@@ -1,4 +1,5 @@
 #pragma once
+#pragma once
 #include <vector>
 #include <set>
 #include <string>
@@ -6,9 +7,13 @@ using namespace std;
 class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> reslut;
+        //映射皇后位置和不能再摆放位置
         vector<vector<int>> mark;
+        //当前可能的棋盘摆法
         vector<string> location;
+        //最后结果
+        vector<vector<string>> result;
+        //给vector生成一下空间
         for (int i = 0; i < n; i++) {
             mark.push_back(vector<int>());
             for (int j = 0; j < n; j++) {
@@ -17,8 +22,9 @@ public:
             location.push_back("");
             location[i].append(n, '.');
         }
-        generate(0, n, location, reslut, mark);
-        return reslut;
+        //从第一行棋盘开始尝试放置
+        generate(0, n, location, result, mark);
+        return result;
     }
 private:
     void put_down_the_queen(int x, int y, vector<vector<int>>& mark) {
@@ -27,6 +33,7 @@ private:
         mark[x][y] = 1;
         for (int i = 1; i < mark.size(); i++) {
             for (int j = 0; j < 8; j++) {
+                //所有不能再放的位置设1
                 int new_x = x + i * dx[j];
                 int new_y = y + i * dy[j];
                 if (new_x >= 0 && new_x < mark.size() && new_y >= 0 && new_y < mark.size()) {
@@ -35,21 +42,60 @@ private:
             }
         }
     }
-    void generate(int k, int n, vector<string>& location, vector<vector<string>>& reslut, vector<vector<int>>& mark) {
-        if (k == n){
-            reslut.push_back(location);        
+    void generate(int k, int n, vector<string>& location, vector<vector<string>>& result, vector<vector<int>>& mark) {
+        //回溯模板,已经放了n个皇后了
+        if (k == n) {
+            result.push_back(location);
             return;
-    }
-    for (int i = 0; i < n; i++) {
-        if (mark[k][i] == 0) {
-            vector<vector<int>> tmp = mark;
+        }
+        //从上到下，放皇后
+        for (int i = 0; i < n; i++) {
+            auto tmp = mark;
             mark[k][i] = 1;
             location[k][i] = 'Q';
             put_down_the_queen(k, i, mark);
-            generate(k + 1, n, location, reslut, mark);
+            generate(k + 1, n, location, result, mark);
+            //回溯
             mark = tmp;
             location[k][i] = '.';
         }
     }
-}
+};
+
+//leetcode上时间复杂度更好的解法，基于位运算的回溯
+class Solution2 {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        auto solutions = vector<vector<string>>();
+        auto queens = vector<int>(n, -1);
+        solve(solutions, queens, n, 0, 0, 0, 0);
+        return solutions;
+    }
+
+    void solve(vector<vector<string>> &solutions, vector<int> &queens, int n, int row, int columns, int diagonals1, int diagonals2) {
+        if (row == n) {
+            auto board = generateBoard(queens, n);
+            solutions.push_back(board);
+        } else {
+            int availablePositions = ((1 << n) - 1) & (~(columns | diagonals1 | diagonals2));
+            while (availablePositions != 0) {
+                int position = availablePositions & (-availablePositions);
+                availablePositions = availablePositions & (availablePositions - 1);
+                int column = __builtin_ctz(position);
+                queens[row] = column;
+                solve(solutions, queens, n, row + 1, columns | position, (diagonals1 | position) >> 1, (diagonals2 | position) << 1);
+                queens[row] = -1;
+            }
+        }
+    }
+
+    vector<string> generateBoard(vector<int> &queens, int n) {
+        auto board = vector<string>();
+        for (int i = 0; i < n; i++) {
+            string row = string(n, '.');
+            row[queens[i]] = 'Q';
+            board.push_back(row);
+        }
+        return board;
+    }
 };
