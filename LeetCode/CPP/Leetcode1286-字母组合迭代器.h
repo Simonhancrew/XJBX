f162dac5a4f8474582d1df6a2ec0517545e203ba
Wuhan <597494370@qq.com>
2020-09-11 07:56:52 +0800

    2020/09/11 daily

---
+++ LeetCode/CPP/Leetcode1286-字母组合迭代器.h
@@ -0,0 +1,16 @@
+#include <string>
+using namespace std;
+class CombinationIterator {
+public:
+    CombinationIterator(string characters, int combinationLength) {
+
+    }
+    
+    string next() {
+
+    }
+    
+    bool hasNext() {
+
+    }
+};
