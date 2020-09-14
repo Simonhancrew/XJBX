f859ba862449c363a3f396e8684da8f90a08a8e1
Simonhancrew <597494370@qq.com>
2020-09-14 11:30:56 +0800

    优化括号匹配

--- LeetCode/CPP/Leetcode20-有效的括号.h
+++ LeetCode/CPP/Leetcode20-有效的括号.h
@@ -2,35 +2,22 @@
 #include<stack>
 #include<string>
 using namespace std;
+//没必要每个括号都入栈，直接入栈应匹配的，到相应位置时将此处括号和栈顶对比
+//空间复杂度会降低
 class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
         for (auto ch : s) {
-            if (ch == '(' || ch == '[') {
-                st.push(ch);
-            }
-            else {
-                if (st.empty()) {
-                    return false;
-                }
-                char top = st.top();
-                if (ch == ')' && top != '(') return false;
-                if (ch == ')' && top == '(') {
-                    st.pop();
-                    continue;
-                }
-                if (ch == ']' && top != '[') return false;
-                if (ch == ']' && top == '[') {
-                    st.pop();
-                    continue;
-                }
-
-            }
+            if(ch == '(') st.push(')');
+            else if(ch == '{') st.push('}');
+            else if(ch == '[') st.push(']');
+            //不匹配或者栈提前变空了
+            else if(st.empty()||st.top() != ch) return false;
+            //匹配成功，弹出
+            else st.pop();
         }
-        if (!st.empty()) {
-            return false;
-        }
-        return true;
+        //结束的时候如果栈不空的话就没有匹配成功
+        return st.empty();
     }
 };
