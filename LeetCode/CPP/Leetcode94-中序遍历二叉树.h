67870dd9a9506fd18fdcaaa5693a5c1b6762e8a0
Simonhancrew <597494370@qq.com>
2020-09-14 11:14:21 +0800

    94压栈

--- LeetCode/CPP/Leetcode94-中序遍历二叉树.h
+++ LeetCode/CPP/Leetcode94-中序遍历二叉树.h
@@ -21,11 +21,22 @@ private:
 };
 
 //栈调用
-
+#include <stack>
 class Solution2 {
 public:    
     vector<int> inorderTraversal(TreeNode* root){
-
+        vector<int> res;
+        stack<TreeNode*> st;
+        while(root !=nullptr||!st.empty()){
+            while(root != nullptr){
+                st.push(root);
+                root = root->left;
+            }
+            root = st.top();
+            st.pop();
+            res.push_back(root->val);
+            root = root->right;
+        }
     }
 };
 
