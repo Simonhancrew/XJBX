e7b6e1446a92beb28e42e62f4acc5427dc2dbc6a
Simonhancrew <597494370@qq.com>
2020-09-17 16:22:39 +0800

    单调队列

---
+++ LeetCode/CPP/Leetcode239-求滑动窗口的最大值.h
@@ -0,0 +1,51 @@
+#include <vector>
+#include <deque>
+using namespace std;
+//形如滑动窗口求最值，考虑单调队列单调栈
+//单调队列的应用
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k){
+        singleQue que;
+        vector<int> result;
+        for(int i =0;i<k;i++){
+            que.push(nums[i]);
+        }
+        result.push_back(que.front());
+        for(int i = k;i<nums.size();i++){
+            //去掉头元素
+            que.pop(nums[i-k]);
+            //弹入尾元素
+            que.push(nums[i]);
+            result.push_back(que.front());
+        }
+        return result;
+    }
+private:
+    //一个单调队列的push和pop遵从一定的规则
+    //这样能保证队列的头永远是最大的数
+    //是指上这个队列并没有维护所有的值
+    class singleQue{
+    public:
+        deque<int> que;
+        //pop之前检查队列是否为空
+        //检查当前需要弹出的数值是否是队列头的数值，如果是的话就需要弹出
+        void pop(int value){
+            if(!que.empty() && value == que.front()){
+                que.pop_front();
+            }
+        }
+        //如果push的元素大于front的元素，则将front的元素pop掉
+        //直到push的数值小于等于入口处的数值
+        void push(int value){
+            while(!que.empty() && value>que.front()){
+                que.pop_front();
+            }
+            que.push_back(value);
+        }
+
+        int front(){
+            return que.front();
+        }
+    };
+};
