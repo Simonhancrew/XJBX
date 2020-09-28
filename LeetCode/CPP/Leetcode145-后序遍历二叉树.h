62d270b1717175ac756eca5f9a15f6d2f4b24afc
Wuhan <597494370@qq.com>
2020-09-29 07:48:10 +0800

    二叉树的后序遍历

--- LeetCode/CPP/Leetcode145-后序遍历二叉树.h
+++ LeetCode/CPP/Leetcode145-后序遍历二叉树.h
@@ -13,12 +13,28 @@ public:
         while(!stk.empty()){
             auto tmp = stk.top();
             stk.pop();
+            //遇子叶节点，本次循环结束
             if(tmp!= nullptr) res.push_back(tmp->val);
             else continue;
             stk.push(tmp->left);
             stk.push(tmp->right);
         }
         reverse(res.begin(),res.end());
+        return res;
+    }
+};
+//递归的代码
+class Solution2 {
+public:
+    void DFS(TreeNode* root,vector<int> &res){
+        if(root == nullptr) return;
+        DFS(root->left,res);
+        DFS(root->right,res);
+        res.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        DFS(root,res);
         return res;
     }
 };
