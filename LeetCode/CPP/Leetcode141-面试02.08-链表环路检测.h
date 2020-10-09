a84e1e8f61834dd71cf5c8232bc3ac887cda97fd
Wuhan <597494370@qq.com>
2020-10-09 11:24:32 +0800

    2020/10/09 daily

--- LeetCode/CPP/Leetcode141-面试02.08-链表环路检测.h
+++ LeetCode/CPP/Leetcode141-面试02.08-链表环路检测.h
@@ -1,42 +1,22 @@
-
-using namespace std;
-
-struct ListNode
-{
-    int val;
-    ListNode* next;
-    ListNode(int x):val(x),next(nullptr)} { }
-    /* data */
-};
-
+#include "ADT.hpp"
 class Solution {
 public:
-    ListNode *detectCycle(ListNode *head) {
-        ListNode *fast = head;
-        ListNode *slow = head;
-        ListNode *meet = nullptr;
+    bool hasCycle(ListNode *head) {
+        ListNode* slow;
+        ListNode* fast;
+        slow = head;
+        fast =head;
         while(fast){
-            fast = fast->next;
             slow = slow->next;
-            if(!fast){
-                return nullptr;
+            fast = fast->next;
+            if(fast ==nullptr){
+                return false;
             }
             fast = fast->next;
             if(fast == slow){
-                meet = fast;
-                break;
+                return true;
             }
         }
-        if(meet == nullptr){
-            return nullptr;
-        }
-        while(head && meet){//从头节点和快慢指针相交节点一起同速度出发，相交处就是环的起点
-            if(meet == head){
-                return head;
-            }
-            head = head->next;
-            meet = meet->next;
-        }
-        return nullptr;
+        return false;
     }
 };
