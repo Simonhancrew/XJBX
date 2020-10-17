ac69566a49c27c9ca6b92f27168fab1488bdbf59
Wuhan <597494370@qq.com>
2020-10-17 13:05:24 +0800

    2020/10/17 daily

--- LeetCode/CPP/Leetcode236-二叉树的最近公共祖先.h
+++ LeetCode/CPP/Leetcode236-二叉树的最近公共祖先.h
@@ -1,6 +1,7 @@
 #include "ADT.hpp"
 #include <vector>
 using namespace std;
+//前序找到路径，遍历这个较短的路径，找到相同的最后一个node
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
@@ -40,6 +41,29 @@ private:
         preorder(node->left,search,path,result,finish);
         preorder(node->right,search,path,result,finish);
         path.pop_back();
+
+    }
+};
+
+//后序天然回溯
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        //从子节点穿node回上层
+        if (root == q || root == p || root == NULL) return root;
+        //左子树和右子树传回来的值
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        //都不空，则这个节点就是公共，往上传
+        if (left != NULL && right != NULL) return root;
+        //左空，从右侧传上来的，继续往上传
+        if (left == NULL && right != NULL) return right;
+        //右空，从左侧传上来的
+        else if (left != NULL && right == NULL) return left;
+        //都空，找不到这两个节点
+        else  { //  (left == NULL && right == NULL)
+            return NULL;
+        }
 
     }
 };
