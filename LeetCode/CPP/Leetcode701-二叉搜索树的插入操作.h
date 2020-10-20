6d42a079cc71d9a47e4942ff6f5433c7324cb976
Wuhan <597494370@qq.com>
2020-10-20 13:17:17 +0800

    note

--- LeetCode/CPP/Leetcode701-二叉搜索树的插入操作.h
+++ LeetCode/CPP/Leetcode701-二叉搜索树的插入操作.h
@@ -29,12 +29,16 @@ public:
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
+    //终止条件就是找到遍历的节点为null的时候，就是要插入节点的位置了，并把插入的节点返回。
         if (root == nullptr) {
             TreeNode* node = new TreeNode(val);
             return node;
         }
+        //搜索树是有方向了，可以根据插入元素的数值，决定递归方向。
         if (root->val > val) root->left = insertIntoBST(root->left, val);
         if (root->val < val) root->right = insertIntoBST(root->right, val);
+
+        //上一层将加入节点返回，本层用root->left或者root->right将其接住
         return root;
     }
 };
