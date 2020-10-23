b2def2ba4fd7d44ca9a8dd45a833fdd9e73d3082
Wuhan <597494370@qq.com>
2020-10-23 13:31:58 +0800

    daily加餐

--- LeetCode/CPP/Leetcode108-有序数组转成二叉树.h
+++ LeetCode/CPP/Leetcode108-有序数组转成二叉树.h
@@ -21,7 +21,8 @@ public:
         }
 
         // 总是选择中间位置左边的数字作为根节点
-        int mid = (left + right) / 2;
+        //考虑越界的问题
+        int mid = left + (right-left)/2;
 
         TreeNode* root = new TreeNode(nums[mid]);
         root->left = helper(nums, left, mid - 1);
@@ -30,7 +31,48 @@ public:
     }
 };
 
+//迭代的方法
+#include <queue>
+using std::queue;
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        if (nums.size() == 0) return nullptr;
 
+        TreeNode* root = new TreeNode(0);   // 初始根节点
+        queue<TreeNode*> nodeQue;           // 放遍历的节点
+        queue<int> leftQue;                 // 保存左区间下表
+        queue<int> rightQue;                // 保存右区间下表
+        nodeQue.push(root);                 // 根节点入队列
+        leftQue.push(0);                    // 0为左区间下表初始位置
+        rightQue.push(nums.size() - 1);     // nums.size() - 1为右区间下表初始位置
+
+        while (!nodeQue.empty()) {
+            TreeNode* curNode = nodeQue.front();
+            nodeQue.pop();
+            int left = leftQue.front(); leftQue.pop();
+            int right = rightQue.front(); rightQue.pop();
+            int mid = left + ((right - left) / 2);
+
+            curNode->val = nums[mid];       // 将mid对应的元素给中间节点
+
+            if (left <= mid - 1) {          // 处理左区间
+                curNode->left = new TreeNode(0);
+                nodeQue.push(curNode->left);
+                leftQue.push(left);
+                rightQue.push(mid - 1);
+            }
+
+            if (right >= mid + 1) {         // 处理右区间
+                curNode->right = new TreeNode(0);
+                nodeQue.push(curNode->right);
+                leftQue.push(mid + 1);
+                rightQue.push(right);
+            }
+        }
+        return root;
+    }
+};
 
 
 class Solution2 {
