4c5ae65388bb7a937d0624bc3e034a747e5e6ace
Simonhancrew <597494370@qq.com>
2020-10-30 10:20:54 +0800

    回溯的宽度和深度

--- LeetCode/CPP/Leetcode17-电话号码的组合.h
+++ LeetCode/CPP/Leetcode17-电话号码的组合.h
@@ -2,7 +2,6 @@
 #include<string>
 #include<unordered_map>
 using namespace std;
-//最好到临界区加回溯，然后找准进入result的条件
 //更优化空间可以自己压栈
 /* 一个回溯的模板
 backtracking() {
@@ -17,17 +16,27 @@ backtracking() {
     }
 }
 */
-//循环可以加到递归函数里，没必要放外面
-//
+//首先一个字母和数字的映射，找到回溯的宽度
+//然后回溯的深度就是数字的个数
+//其实还应该考虑错误的数字，比如按1和* #，但测试数据没有这种
 class Solution{
-public:
+public: 
     vector<string> letterCombinations(const string &digits){
-        vector<string> result;
+        cur.clear();
+        result.clear();
         if(digits.empty()){
             return result;
         }
-        string cur = "";
-        unordered_map<char,string> Digit2Alpha{
+        BackTrait(digits,0);
+        return result;
+    }
+private:
+    //当前的可行字符
+    string cur = "";
+    //储存最后的结果
+    vector<string> result;
+    //数字字母映射
+    unordered_map<char,string> Digit2Alpha{
             {'2', "abc"},
             {'3', "def"},
             {'4', "ghi"},
@@ -37,19 +46,22 @@ public:
             {'8', "tuv"},
             {'9', "wxyz"}
         };
-        BackTrait(digits,cur,Digit2Alpha,result,0);
-        return result;
-    }
-private:
-    void BackTrait(const string &digits,string &cur,const unordered_map<char,string> &Digit2Alpha,vector<string> &result,int index){
+    //回溯
+    void BackTrait(const string &digits,int index){
+        //终止条件
         if(index == digits.size()){
             result.push_back(cur);
+            return;
         }else{
+            //当前的数字
             char digit = digits[index];
+            //映射的可能字母
             const string &letters = Digit2Alpha.at(digit);
+            //宽度回溯
             for(const char& letter:letters){
                 cur.push_back(letter);
-                BackTrait(digits,cur,Digit2Alpha,result,index+1);
+                //深度回溯
+                BackTrait(digits,index+1);
                 cur.pop_back();
             }
         }
