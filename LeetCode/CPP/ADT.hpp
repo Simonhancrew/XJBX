b78d69481f03fbc76ad76a7fe6cb810a0f589063
Wuhan <597494370@qq.com>
2020-10-31 18:50:55 +0800

    2020/10/31 daily

--- LeetCode/CPP/ADT.hpp
+++ LeetCode/CPP/ADT.hpp
@@ -6,7 +6,7 @@ struct ListNode {
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
- };
+};
 
 struct TreeNode {
     int val;
