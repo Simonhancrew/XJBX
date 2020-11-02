552e0ee327b6b451e0bfec8112319e0b7a497bcb
Wuhan <597494370@qq.com>
2020-11-02 21:03:00 +0800

    动态规划专题

--- LeetCode/CPP/Leetcode55-跳跃游戏.h
+++ LeetCode/CPP/Leetcode55-跳跃游戏.h
@@ -21,3 +21,30 @@ public:
         return false;
     }
 };
+//动态规划
+//最后一步，假设能跳到
+//回到之前的一步
+class Solution {
+public:
+    bool canJump(vector<int>& nums){
+        if(nums.size() == 0){
+            return false;
+        }
+        vector<bool> dp;
+        for(int i =0;i<nums.size();++i){
+            dp.push_back(false);
+        }
+        dp[0] = true;
+        //对于之后的数
+        for(int j = 1;j<nums.size();j++){
+            //如果之前有数能跳到
+            for(int i =0;i < j;++i){
+                if(dp[i] && i+nums[i] >= j){
+                    dp[j] = true;
+                    break;
+                }
+            }
+        }
+        return dp[nums.size() - 1];
+    }
+};
