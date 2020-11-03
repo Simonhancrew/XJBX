5942d70e3069d8e01dea28a29d8df629d921f880
Wuhan <597494370@qq.com>
2020-11-03 19:45:27 +0800

    动态规划专题

---
+++ LeetCode/CPP/Leetcode62-不同的路径.h
@@ -0,0 +1,23 @@
+#include <vector>
+using std::vector;
+//动态规划
+//最后一步，能到达最后
+//退后一步，形成递推
+//考虑边界条件
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int> (n,0));
+        dp[0][0] = 1;
+        for(int i = 0;i < m;++i){
+            for(int j = 0;j < n;++j){
+                if(i == 0 ||j == 0){
+                    dp[i][j] = 1;
+                }else{
+                    dp[i][j] = dp[i-1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
