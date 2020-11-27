f96122e91096fc894410c31f148b7ae970669ba1
Wuhan <597494370@qq.com>
2020-11-27 22:13:21 +0800

    划分型动态规划

---
+++ LeetCode/CPP/Leetcode279-完全平方数.h
@@ -0,0 +1,26 @@
+#include <vector>
+using namespace std;
+//最后一步的最优解
+//一定会有一个j*j减完
+//那么剩下的n-j*j的最优解？
+//dp[i] -> 数字i可以被多少完全平方数组成
+//dp[i] = min(dp[i - j*j] + 1),其中，1=<j*j<=i
+class Solution {
+public:
+    int numSquares(int n) {
+        //划分型动态规划
+        vector<int> dp(n + 1,INT_MAX);
+        //0可以被0个完全平方数拼成
+        dp[0] = 0;
+        for(int i = 1;i <= n;++i){
+            //对于每一个i，求出他的最优解
+            //j*j必须小于i
+            for(int j = 1;j * j <= i;++j){
+                if(dp[i - j*j] + 1 < dp[i]){
+                    dp[i] = dp[i - j*j] + 1;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
