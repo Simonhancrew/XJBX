0c0fb1660992378aac474df9dc7396ec7434243d
Wuhan <597494370@qq.com>
2020-12-01 12:03:44 +0800

    2020/12/1 daily 加深理解二分

--- LeetCode/CPP/Leetcode34-在排序数组中查找元素的第一个和最后一个位置.h
+++ LeetCode/CPP/Leetcode34-在排序数组中查找元素的第一个和最后一个位置.h
@@ -1,38 +1,34 @@
 #pragma once
 #include <vector>
 using namespace std;
-class solution {
+class Solution {
 public:
-	vector<int> searchrange(vector<int>& nums, int target) {
-		vector<int> result;
-		result.push_back(left_bound(nums, target));
-		result.push_back(right_bound(nums, target));
-		return result;
-	}
+    vector<int> searchRange(vector<int>& nums, int target) {
+        return vector<int>{leftBound(nums,target),rightBound(nums,target)};
+    }
 private:
-	int left_bound(vector<int>& nums, int target) {
+    int leftBound(vector<int> &nums,int target){
+        int begin = 0,end = nums.size() - 1;
+		//二分查找的细节，如果时闭区间的话，要用<=
+        while(begin <= end){
+            int mid = begin + ((end - begin) >> 1);
+            if(nums[mid] == target){
+                if(mid == 0 ||nums[mid - 1] < target){
+                    return mid;
+                }
+                end = mid - 1;
+            }else if(nums[mid] < target){
+                begin = mid + 1;
+            }else if(nums[mid] > target){
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+	int rightBound(vector<int>& nums, int target) {
 		int begin = 0, end = nums.size() - 1;
 		while (begin <= end) {
-			int mid = (begin + end) / 2;
-			if (target == nums[mid]) {
-				if (mid == 0 || nums[mid-1]<target) {
-					return mid;
-				}
-				end = mid - 1;
-			}
-			else if (nums[mid] > target) {
-				end = mid - 1;
-			}
-			else if (nums[mid] < target) {
-				begin = mid + 1;
-			}		
-		}
-		return -1;
-	}
-	int right_bound(vector<int>& nums, int target) {
-		int begin = 0, end = nums.size() - 1;
-		while (begin <= end) {
-			int mid = (end + begin) / 2;
+			int mid = begin + ((end - begin) >> 1);
 			if (nums[mid] == target) {
 				if (mid == nums.size() - 1|| nums[mid+1]>target) {
 					return mid;
@@ -47,5 +43,5 @@ private:
 			}
 		}
 		return -1;
-	}
+	}    
 };
