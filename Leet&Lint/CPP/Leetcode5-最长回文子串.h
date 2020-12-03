f3a23e8bee6c1d4b89039074ba1124682fc00578
Wuhan <597494370@qq.com>
2020-12-03 18:56:02 +0800

    区间动态规划

---
+++ Leet&Lint/CPP/Leetcode5-最长回文子串.h
@@ -0,0 +1,66 @@
+#include <vector>
+#include <string>
+using namespace std;
+//区间型动态规划
+//找回文串长度类的，找到始末位置，要设置成dp[i][j]->i-j位置能不能构成字符串
+//要注意动态规划的状态顺序
+//我们从较短向较长的过度的
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        string ans = "";
+        vector<vector<int>> dp(n,vector<int>(n,false));
+        //l是距离，i是坐标
+        for (int l = 0; l < n; ++l) {
+            for (int i = 0; i + l < n; ++i) {
+                int j = i + l;
+                if (l == 0) {
+                    dp[i][j] = 1;
+                } else if (l == 1) {
+                    dp[i][j] = (s[i] == s[j]);
+                } else {
+                    dp[i][j] = (s[i] == s[j] && dp[i + 1][j - 1]);
+                }
+                if (dp[i][j] && l + 1 > ans.size()) {
+                    ans = s.substr(i, l + 1);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+//对于每一个位置，我们扩充，看看能不能扩成回文串
+//回文串分为偶数长度和奇数长度的，在这里分别处理
+//然后相对的维护最长的ans
+class Solution{
+public:
+    string longestPalindrome(string s){
+        int n = s.size();
+        string ans = "";
+        vector<vector<bool>> dp(n,vector<bool> (n,false));
+        for(int t = 0;t < n;++t){
+            //奇数长度的回文串
+            int i = t,j = t;
+            while(i >= 0 && j < n && s[i] == s[j]){
+                dp[i][j] = true;
+                if(ans.size() < j - i + 1){
+                    ans = s.substr(i,j-i+1);
+                }
+                --i,++j;
+            }
+            //偶数长度的回文串
+            i = t,j = t+1;
+            while(i >= 0 && j < n && s[i] == s[j]){
+                dp[i][j] = true;
+                if(ans.size() < j - i + 1){
+                    ans = s.substr(i,j-i+1);
+                }
+                --i,++j;
+            }
+        }
+        return ans;
+    }
+};
