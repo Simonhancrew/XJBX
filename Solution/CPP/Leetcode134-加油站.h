880e42c4aca86e470c79b8f94be7726fcd65d5bd
Wuhan <597494370@qq.com>
2020-12-14 14:10:38 +0800

    区间贪心

--- Solution/CPP/Leetcode134-加油站.h
+++ Solution/CPP/Leetcode134-加油站.h
@@ -1,32 +1,40 @@
 #include <vector>
 using namespace std;
-//可以证明，x-y,y是第一个无法到达的加油站，那么在xy之间的加油站作为下一个起点
-//也是无法做到全部到达的，所以从y+1开始就可以了
+//暴力法
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int n = gas.size();
-        int i = 0;
-        while(i < n){
-            int sumOfGas = 0,sumOfCost = 0;
-            int cnt = 0;
-            while(cnt < n){
-                int j = (i + cnt) % n;
-                sumOfGas += gas[j];
-                sumOfCost += cost[j];
-                //花费大于油，i= i+cnt+1
-                if(sumOfCost > sumOfGas){
-                    break;
-                }
-                ++cnt;
+        for(int i = 0;i < gas.size();++i){
+            int rest = gas[i] - cost[i];
+            int index = (i + 1) % gas.size();
+            //环形循环可以考虑while的trick
+            while(rest > 0 &&index != i){
+                rest += gas[index] - cost[index];
+                index = (index + 1) % gas.size();
             }
-            //cnt == n说明能够完整的走完，结束
-            if(cnt == n){
-                return i;
-            }else{
-                i = i + cnt + 1;
-            }
+            if(rest >= 0 && index == i) return i;
         }
         return -1;
+    }
+};
+
+//所有的gas和如果大于等于cost，则可以找到这个坐标
+//从i最大走到j的话，则在(i,j)之间的坐标是无法走到一圈的，只能重新从j+1开始，可以证明
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int curSum = 0;
+        int totalSum = 0;
+        int start = 0;
+        for(int i = 0;i < gas.size();++i){
+            curSum += gas[i] - cost[i];
+            totalSum += gas[i] - cost[i];
+            if(curSum < 0){
+                start = i + 1;
+                curSum = 0;
+            }
+        }
+        if(totalSum < 0) return -1;
+        return start;
     }
 };
