888ba2636b5b2c24e20d5d0715753f76ed121271
Simonhancrew <597494370@qq.com>
2020-12-21 14:22:58 +0800

    2020/12/21 daily 动态规划坐标型

---
+++ Solution/Go/Leetcode746-最小哈花费爬楼梯.go
@@ -0,0 +1,12 @@
+package Go
+
+func minCostClimbingStairs(cost []int) int {
+	n := len(cost)
+	dp := make([]int, n+1)
+	for i := 2; i <= n; i++ {
+		dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2])
+	}
+	return dp[n]
+}
+
+//可以用滚动优化
