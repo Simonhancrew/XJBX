2df6fb14b95ba76aa95628c7f7f4e7fbc4aa90a1
han <597494370@qq.com>
2020-12-28 13:00:26 +0800

    2020/12/28 daily

--- Solution/Go/Leetcode738-单调递增的数字.go
+++ Solution/Go/Leetcode738-单调递增的数字.go
@@ -7,16 +7,19 @@ import "strconv"
 //就的到一个最大递增数
 func monotoneIncreasingDigits(N int) int {
 	//string无法修改，改成底层的byte数组
-	s:=[]byte(strconv.Itoa(N))
+	s := []byte(strconv.Itoa(N))
 	var n = len(s)
 	t := n
-	for i := n-1;i>=1;i--{
-		if(s[i-1] > s[i]){
+	//为什么从后往前？332，从前往后会影响之前的状态
+	//329，不符合要变成299
+	//这也符合处理的逻辑，从低位考虑到高位
+	for i := n - 1; i >= 1; i-- {
+		if s[i-1] > s[i] {
 			t = i
-			s[i - 1]--
+			s[i-1]--
 		}
 	}
-	for t < n{
+	for t < n {
 		s[t] = '9'
 		t++
 	}
