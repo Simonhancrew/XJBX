3f3b18c4b038d2391df8d100aac39633fed54db2
Simonhancrew <597494370@qq.com>
2021-01-06 14:28:05 +0800

    sbgo

--- Solution/sbgo/Leetcode205-同构字符串.go
+++ Solution/sbgo/Leetcode205-同构字符串.go
@@ -1,16 +1,16 @@
-package go
+package sbgo
 
 func isIsomorphic(s string, t string) bool {
-	if len(s) != len(t){
+	if len(s) != len(t) {
 		return false
 	}
 	s2t := make(map[byte]byte)
 	t2s := make(map[byte]byte)
-	for i,ch := range s{
-		if s2t[byte(ch)] != 0 && byte(s2t[byte(ch)]) != t[i]{
+	for i, ch := range s {
+		if s2t[byte(ch)] != 0 && byte(s2t[byte(ch)]) != t[i] {
 			return false
 		}
-		if t2s[byte(t[i])] != 0 && byte(t2s[byte(t[i])]) != byte(ch){
+		if t2s[byte(t[i])] != 0 && byte(t2s[byte(t[i])]) != byte(ch) {
 			return false
 		}
 		s2t[byte(ch)] = byte(t[i])
