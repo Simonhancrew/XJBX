aa3f59ba4c464583b8a3a3a2a0bdf0ebcc3b4233
han <597494370@qq.com>
2021-01-14 19:01:59 +0800

    格式的修改

--- Solution/sbgo/Leetcode1018-能被5整除的二进制前缀.go
+++ Solution/sbgo/Leetcode1018-能被5整除的二进制前缀.go
@@ -3,15 +3,15 @@ package sbgo
 func prefixesDivBy5(A []int) []bool {
 	n := len(A)
 	cur := 0
-	res := make([]bool,n)
-	for i := range A{
+	res := make([]bool, n)
+	for i := range A {
 		cur <<= 1
 		if cur >= 10 {
-			cur -= 10 
+			cur -= 10
 		}
 		num := A[i]
 		cur += num
-		if cur % 5 == 0{
+		if cur%5 == 0 {
 			res[i] = true
 		}
 	}
--- Solution/sbgo/Leetcode343-整數拆分.go
+++ Solution/sbgo/Leetcode343-整數拆分.go
@@ -5,14 +5,14 @@ package sbgo
 //于是就形成了递推
 
 func integerBreak(n int) int {
-	dp := make([]int,n+1)
+	dp := make([]int, n+1)
 	dp[2] = 1
-	for i:=3;i<=n;i++{
+	for i := 3; i <= n; i++ {
 		cur := 0
-		for j := 1;j < i;j++{
+		for j := 1; j < i; j++ {
 			//加入cur的原因是如果此轮i中,之前的j拆分的更大
 			//不要丢失在i循环内此时的的最大值
-			cur = max(max(cur,j * (i - j)),dp[i-j] * j)
+			cur = max(max(cur, j*(i-j)), dp[i-j]*j)
 		}
 		dp[i] = cur
 	}
