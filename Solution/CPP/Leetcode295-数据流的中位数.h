6cf1247562a8e9472454216922bc819ae1752bee
han <597494370@qq.com>
2021-01-17 14:02:31 +0800

    最小堆的容器和返回类型修改

--- Solution/CPP/Leetcode295-数据流的中位数.h
+++ Solution/CPP/Leetcode295-数据流的中位数.h
@@ -1,21 +1,29 @@
 #include <queue>
 using namespace std;
-class Solution{
-    priority_queue<int> small_queue;
-    priority_queue<int,greater<int>> big_queue;
+//最小堆储存大的一半的数，最大堆储存小的一般的数
+//保证小的一半数字所在的堆时比更大的一半的堆多一的
+class MedianFinder {
 public:
-    void addNum(int num){
-        small_queue.push(num);
-        big_queue.push(small_queue.top());
-        small_queue.pop();
-        if(small_queue.size()<big_queue.size()){
-            small_queue.push(big_queue.top());
-            big_queue.pop();
+    /** initialize your data structure here. */
+    MedianFinder() {
+
+    }
+    priority_queue<int> small;
+    priority_queue<int,vector<int>,greater<int>> big;
+    void addNum(int num) {
+        small.push(num);
+        big.push(small.top());
+        small.pop();
+        if(small.size() < big.size()){
+            small.push(big.top());
+            big.pop();
         }
     }
-    double findMedian(){
-        return small_queue.size()>big_queue.size()?small_queue.top():(small_queue.top()+big_queue.top())>>1;
+    
+    double findMedian() {
+        if(small.size() > big.size()){
+            return small.top();
+        }
+        return (double)(small.top() + big.top())/ 2;
     }
-
-
 };
