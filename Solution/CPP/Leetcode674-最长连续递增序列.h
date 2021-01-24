575756eb02bf052c46108ff0aad82208fd1f29d1
han <597494370@qq.com>
2021-01-24 13:06:22 +0800

    2021/01/24 daily MaxLis

--- Solution/CPP/Leetcode674-最长连续递增序列.h
+++ Solution/CPP/Leetcode674-最长连续递增序列.h
@@ -29,3 +29,20 @@ public:
         return result;
     }
 };
+
+//小的时候才换头start
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        int ans = 0;
+        int n = nums.size();
+        int start = 0;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] <= nums[i - 1]) {
+                start = i;
+            }
+            ans = max(ans, i - start + 1);
+        }
+        return ans;
+    }
+};
