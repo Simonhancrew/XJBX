cd0a3d0779ae85cd672000a7d9c57663e673020d
han <597494370@qq.com>
2021-02-25 19:20:32 +0800

    use 11 randon

--- XJBX/SCL/Random/KnuthShuffle.h
+++ XJBX/SCL/Random/KnuthShuffle.h
@@ -7,8 +7,9 @@ using namespace std;
 void KnuthShuffle(vector<int> &nums){
     int n = nums.size();
     random_device r;
+    std::mt19937 gen(r);
     for(int i = n-1;i>=0;i--){
         uniform_int_distribution<int> uniform(0,i);
-        swap(nums[i],nums[uniform(r)]);
+        swap(nums[i],nums[uniform(gen)]);
     }
 }
