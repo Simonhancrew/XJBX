1fc2ff0142bb90a9facb2628d53437641f69c9b2
han <597494370@qq.com>
2021-03-01 15:35:39 +0800

    高精度

--- Solution/CPP/Leetcode415-字符串相加.h
+++ Solution/CPP/Leetcode415-字符串相加.h
@@ -19,3 +19,28 @@ public:
         return ans;
     }
 };
+
+#include <vector>
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        string ans = "";
+        vector<int> a,b;
+        for(int i = num1.size() - 1;i >= 0;i--) a.push_back(num1[i] - '0');
+        for(int i = num2.size() - 1;i >= 0;i--) b.push_back(num2[i] - '0');
+
+        vector<int> c;
+        int t = 0;
+        for(int i = 0;i < a.size() || i < b.size();i++){
+            if (i < a.size()) t += a[i];
+            if (i < b.size()) t += b[i];
+            c.push_back(t % 10);
+            t /= 10; 
+        }
+        if(t) c.push_back(1);
+        for(int i = c.size() - 1;i >=0 ;--i){
+            ans.push_back(c[i] + '0');
+        }   
+        return ans;
+    }
+};
