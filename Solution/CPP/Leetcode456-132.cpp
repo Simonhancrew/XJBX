fce59d3393fbf83156ba48f20874ea8cc24800f3
han <597494370@qq.com>
2021-03-24 22:30:27 +0800

    231模式的stack sort

--- Solution/CPP/Leetcode456-132.cpp
+++ Solution/CPP/Leetcode456-132.cpp
@@ -32,6 +32,28 @@ public:
 };
 
 //枚举ai,判断ai的后面是否存在ak存在aj,符合要求
+//一种很优美的解法，记录了2，2右边一定有一个3，枚举的每一个数，如果小于2，那就找到了1
+//利用单调栈去记录3（可以理解成不合格的2）
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        int n = nums.size();
+        if(n < 3){
+            return false;
+        }
+        int two = INT_MIN;
+        stack<int> stk;
+        for(int i = n-1;i >=0;i--){
+            if(nums[i] < two) return true;
+            while(stk.size() && nums[i] > stk.top()){
+                two = stk.top();
+                stk.pop();
+            }
+            stk.push(nums[i]);
+        }
+        return false;
+    }
+};
 
 int main(){
     vector<int> c = {1,2,3,4};
