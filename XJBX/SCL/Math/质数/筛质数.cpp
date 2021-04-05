528bf1faa813d0e6b31e09ca8c818354d08ba539
Simonhancrew <597494370@qq.com>
2021-04-05 12:11:19 +0800

    线性筛的思想求欧拉函数

--- XJBX/SCL/Math/质数/筛质数.cpp
+++ XJBX/SCL/Math/质数/筛质数.cpp
@@ -20,8 +20,10 @@ void get_prime(int n){
 
 //线性筛法
 /*
-n只会被他的最小质因子筛掉
 
+n只会被他的最小质因子筛掉，这是线性筛的核心
+只有质数的倍数才会被筛掉
+1-n中有n/ln(n)个质数
 
 */
 void get_prime_linear(int n){
@@ -29,10 +31,12 @@ void get_prime_linear(int n){
         //如果是质数就记录下来
         if(!st[i]) primes[cnt++] = i;
         //从大到小枚举质数，pj一定小于i的质因子
-        for(int j = 0;primes[j] <= n / i;j++){
-            //每一次把i和质数的乘筛掉
+        //保证primes[j]一定是最小的
+        for(int j = 0;primes[j] <= n / i;j++){//pj * i <= n，pj * i小于n才有意义
+            //每一次把i和质数的乘筛掉，此时pj不是i的质因子，pj一定小于所有i的值因子（从小到大枚举的质因子）
             st[primes[j] * i] = true;
-            if(i % primes[j] == 0) break; //primes[j]一定是i的最小质因子
+            //primes[j]一定是i的最小质因子，因为是从小到大枚举每个质数的
+            if(i % primes[j] == 0) break; 
         }
     }
 }
