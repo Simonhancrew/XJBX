04f0c84e37bdbf3c3b6631ff53f32200ad6af63c
simonhancrew <597494370@qq.com>
2021-04-06 19:45:13 +0800

    unique的更优雅的code

--- Solution/CPP/Leetcode80-删除排序数组中的重复项II.h
+++ Solution/CPP/Leetcode80-删除排序数组中的重复项II.h
@@ -23,3 +23,18 @@ public:
         return ptr;
     }
 };
+
+//unique,更优雅的写法，k指向新数组的末尾，只要新的元素不等于nums[k-1]和nums[k]就可以插入
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() < 3) return nums.size();
+        int k = 1;
+        for(int i = 2;i < nums.size();i++){
+            if(nums[i] != nums[k - 1]){
+                nums[++k] = nums[i];
+            }
+        }
+        return k+1;
+    }
+};
