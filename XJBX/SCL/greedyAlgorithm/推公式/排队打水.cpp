b115a5c9056c5c612cd0a1285c384eb7e7f423a2
simonhancrew <597494370@qq.com>
2021-04-10 17:26:36 +0800

    排序不等式

---
+++ XJBX/SCL/greedyAlgorithm/推公式/排队打水.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+using LL = long long;
+
+const int N = 1e5 + 10;
+int d[N];
+
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i = 0;i < n;i++){
+        cin >> d[i];
+    }
+    sort(d,d + n);
+    LL res = 0;
+    //每一项都会让后面的人等待
+    for(int i = 0;i < n;i++) res += d[i] * (n - i - 1);
+    cout << res << endl;
+    return 0;
+}
