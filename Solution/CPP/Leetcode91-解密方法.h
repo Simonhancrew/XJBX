307b70c51d995ce069523c5381eaf4b687667b40
simonhancrew <597494370@qq.com>
2021-04-21 23:29:48 +0800

    2021/04/21 daily

--- Solution/CPP/Leetcode91-解密方法.h
+++ Solution/CPP/Leetcode91-解密方法.h
@@ -30,3 +30,23 @@ public:
         return dp[n];
     }
 };
+
+//dp要用到i-1状态的话一般多开
+
+class Solution2 {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        s = ' ' + s;
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        for(int i = 1;i <= n;i++){
+            if(s[i] >= '1' && s[i] <= '9') dp[i] += dp[i - 1];
+            if(i > 1){
+                int t = (s[i- 1] - '0') * 10 + s[i] - '0';
+                if(t >= 10 && t <= 26) dp[i] += dp[i - 2];
+            }
+        }
+        return dp[n];
+    }
+};
