94e1786f61ac667355d09d889076ad47072f2cb9
simonhancrew <597494370@qq.com>
2021-04-21 17:26:46 +0800

    socket监听

--- XJBX/Server/basicAPI/README.md
+++ XJBX/Server/basicAPI/README.md
@@ -1,3 +1,5 @@
 ## 一些基础的Linux服务器的api和工具
 
 - [端序](judgeEndian.c):一个整数在内存排列中的顺序将影响他被加载成为整数的值，一般pc选用的都是小端序。而网络序一般都是大端序。一般我们默认把发送的数据转换成为大端序之后再去发送，接收端总是采用大端序接收，考虑需不需要转换的问题。大端序一般是高位放在内存的低位。linux的netinet/in.h提供了4个函数完成端序的转换，端序的判断和系统转换接口详见[judgeEndian.c](judgeEndian.c)
+
+- [socket监听](socketListen.c):初始socket，命名socket，调用listen监听socket。listen中的backlog提示监听队列的最大长度，超过这个长度不接受新的客户连接。
---
+++ XJBX/Server/basicAPI/socketListen.c
@@ -0,0 +1,72 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+/*
+1 通用socket地址
+struct sockaddr{
+    sa_family_t sa_family; //地址族类型
+    char sa_data[14];   //存放socket的地址值
+}
+因为14字节的sa_data基本无法容纳多数的协议族地址，所以有
+
+struct sockaddr_storage
+  {
+    __SOCKADDR_COMMON (ss_);	// Address family, etc. 
+    char __ss_padding[_SS_PADSIZE];
+    __ss_aligntype __ss_align;	// Force desired alignment. 
+  };
+
+*/
+
+
+static bool stop = false;
+
+static void handle_term(int sig){
+    stop = true;
+}
+
+int main(int argc,char *argv[]){
+    signal(SIGTERM,handle_term);
+
+    if(argc < 3){
+        printf("usgae:%s ip_address port_number backlogh\n",basename[argv[0]]);
+        return 1;
+    }
+    const char *ip = argv[1];
+    int port = atoi(argv[2]);
+    int backlog = atoi(argv[3]);
+
+    int sock = socket(PF_INET,SOCK_STREAM,0);
+    assert(sock > 0);
+
+    //创建一个ipv4 socket地址
+    struct sockaddr_in address;
+    bzero(&address,sizeof address);
+    address.sin_family = AF_INET;
+    inet_pton(AF_INET,ip,address.sin_addr);//present 2 number
+    address.sin_port = htons(port);//变网络序
+
+    //命名socket，给他地址绑定
+    int ret = bind(sock,(struct sockaddr *)&address,sizeof address);
+    assert(res != -1);
+
+    ret = listen(sock,backlog);
+    assert(res != -1);
+
+    //循环等待，直到kill他
+    while(!stop){
+        sleep(1);
+    }
+    close(sock);
+    return 0;
+}
+
+
+
