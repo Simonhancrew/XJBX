460da2215153313ee6659e0c4e681adab88f9ff0
simonhancrew <597494370@qq.com>
2021-06-18 16:43:43 +0800

    annotation

--- Solution/CPP/Leetcode483-最好小进制.h
+++ Solution/CPP/Leetcode483-最好小进制.h
@@ -2,6 +2,11 @@
 
 using namespace std;
 
+// 根据题目的范围，[3,1e18],首先确定n是可以爆int的（1e9以内）
+// 之后我们确定一下位数，t = log2(n) + 1是最大的长度，进制最小是2进制
+// 显然t越长，k越小，所以可以倒着枚举
+// 然后可以根据二项公式推出来一个范围k = int(pow(n,1 / (t - 1)))
+
 class Solution {
 public:
     string smallestGoodBase(string n) {
